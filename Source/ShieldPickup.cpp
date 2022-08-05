45d3b34e02c0174e03c5df9d222b9fbe49973d23
SaulCondoriM <104391550+SaulCondoriM@users.noreply.github.com>
2022-08-05 12:46:12 -0500

    Add files via upload

---
+++ Source/ShieldPickup.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShieldPickup.h"
+#include "Blaster/Character/BlasterCharacter.h"
+#include "Blaster/BlasterComponents/BuffComponent.h"
+
+void AShieldPickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+	ABlasterCharacter* BlasterCharacter = Cast<ABlasterCharacter>(OtherActor);
+	if (BlasterCharacter)
+	{
+		UBuffComponent* Buff = BlasterCharacter->GetBuff();
+		if (Buff)
+		{
+			try
+			{
+				Buff->ReplenishShield(ShieldReplenishAmount, ShieldReplenishTime);
+			}
+			catch(const std::exception& e)
+			{
+				UE_LOG(LogTemp, Error, TEXT("Error con el buff de ESCUDO"), *e.what());
+			}
+		}
+	}
+
+	Destroy();
+}
---
+++ Source/SpeedPickup.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SpeedPickup.h"
+#include "Blaster/Character/BlasterCharacter.h"
+#include "Blaster/BlasterComponents/BuffComponent.h"
+
+void ASpeedPickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+	ABlasterCharacter* BlasterCharacter = Cast<ABlasterCharacter>(OtherActor);
+	if (BlasterCharacter)
+	{
+		UBuffComponent* Buff = BlasterCharacter->GetBuff();
+		if (Buff)
+		{
+
+			try
+			{
+				Buff->BuffSpeed(BaseSpeedBuff, CrouchSpeedBuff, SpeedBuffTime);
+			}
+			catch(const std::exception& e)
+			{
+				UE_LOG(LogTemp, Error, TEXT("Error con el buff de VELOCIDAD"), *e.what());
+			}
+		}
+	}
+
+	Destroy();
+}
