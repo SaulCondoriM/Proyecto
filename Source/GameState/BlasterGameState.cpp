ec0e28b131c37d94f05941c7cf78c568427da46d
SaulCondoriM <104391550+SaulCondoriM@users.noreply.github.com>
2022-08-24 13:37:07 -0500

    BlasterGameState

--- Source/GameState/BlasterGameState.cpp
+++ Source/GameState/BlasterGameState.cpp
@@ -1,6 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "BlasterGameState.h"
 #include "Net/UnrealNetwork.h"
 #include "Blaster/PlayerState/BlasterPlayerState.h"
